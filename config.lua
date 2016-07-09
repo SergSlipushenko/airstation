b07830442145f9fe3a714ad66d64d182b6b864cf
sslypushenko <sslypushenko@mirantis.com>
2016-07-09 14:16:59 +0300

    +++

---
+++ config.lua
@@ -0,0 +1,9 @@
+local cfg = {
+  verbose =0,
+  ledshow = 1,
+  led_intensity = 64,
+  dsp_contrast =52,
+  cycle = 10000
+}
+
+return cfg
--- main.lua
+++ main.lua
@@ -1,17 +1,18 @@
 dofile('htu21d.lua')
 dofile('dsp.lua')
-co2, pwm, warmup = 0, -1, 0
-verbose, ledshow = 0, 1
+cfg=dofile('config.lua')
+co2, pwm, warmup = 0, -1
 ws2812.init()
 pwm_pin = 6
 gpio.mode(pwm_pin, gpio.INT)
-intn = 64
+intn = cfg.led_intensity
 gled=string.char(intn,0,0)
 rled=string.char(0,intn,0)
 yled=string.char(intn/2,intn/2,0)
 zled=string.char(0,0,0)
 wled=string.char(intn/3,intn/3,intn/3)
 ledlen=8
+dsp:dsp('CNT' .. cfg.dsp_contrast)
 
 function leds(led, back, n, total)
    back = back or zled
@@ -28,19 +29,18 @@ end
 log_data = function()
     temp = htu21d:temp()/10
     hum = htu21d:hum()
-    dsp:dsp('CLR');dsp:dsp('TSZ2');
-    if warmup == 0 and tmr.time() < 200 then
+    dsp:dsp('CLR');dsp:dsp('CNT' .. cfg.dsp_contrast);dsp:dsp('TSZ2');
+    if tmr.time() < 200 then
         dsp:dsp('PRL****');
     else
         dsp:dsp('PRN' .. co2);dsp:dsp('TSZ1');dsp:dsp('PRNppm');dsp:dsp('TSZ2');dsp:dsp('PRL')
-        warmup = 1
-        if verbose>0 then 
+        if cfg.verbose>0 then 
             print('{"time":'..tmr.time()..',"co2":'..co2..',"temp":'..temp..',"hum":'..hum.."}") end
+        if cfg.ledshow>0 then to_level(co2) end
     end
     dsp:dsp('PRL' .. temp/10 ..'.'.. temp%10 .. 'C');dsp:dsp('PRN' .. hum .. '%')
     dsp:dsp('TSZ1');dsp:dsp('CRS4241');dsp:dsp('PRN' .. tmr.time())
     dsp:dsp('CRS4233');dsp:dsp('PRN' .. node.heap());dsp:dsp()
-	if ledshow>0 then to_level(co2) end
 end
 
 ms_callback = function(l) 
@@ -55,6 +55,6 @@ end
 function ms_co2() gpio.trig(pwm_pin, "both", ms_callback) end
 
 function stop() tmr.stop(1) end
-function run() tmr.alarm(1, 10000, tmr.ALARM_AUTO, ms_co2) end
+function run() tmr.alarm(1, cfg.cycle, tmr.ALARM_AUTO, ms_co2) end
 
 ws2812.write(string.rep(gled,2) .. string.rep(yled,2) .. string.rep(rled,2) .. string.rep(wled,2))
