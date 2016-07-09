f86682fb32a4e8f0ef96e1dd4984941431575928
sslypushenko <sslypushenko@mirantis.com>
2016-07-09 14:17:53 +0300

    Set init delay to 3000

--- init.lua
+++ init.lua
@@ -1,1 +1,1 @@
-print('try to boot');tmr.alarm(6, 7000, 0, function() pcall(function() dofile('utils.lua'); safe_main() end) end)
+print('try to boot');tmr.alarm(6, 3000, 0, function() pcall(function() dofile('utils.lua'); safe_main() end) end)
